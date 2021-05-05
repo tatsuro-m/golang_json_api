936fe216fbc62fa1222657206c289e809d6e6b24
Tatsuro Miyamoto <mtatsu0130@gmail.com>
2021-05-05 09:54:18 +0900

    add api

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
-module "golang_json_api"
+module golang_json_api
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Task struct {
+	ID      int       `json:"id"`
+	Title   string    `json:"title"`
+	Content string    `json:"content"`
+	DueDate time.Time `json:"due_date"`
+}
+
+var tasks = []Task{{
+	ID:      1,
+	Title:   "A",
+	Content: "Aタスク",
+	DueDate: time.Now(),
+}, {
+	ID:      2,
+	Title:   "B",
+	Content: "Bタスク",
+	DueDate: time.Now(),
+}, {
+	ID:      3,
+	Title:   "C",
+	Content: "Cタスク",
+	DueDate: time.Now(),
+}}
+
+func main() {
+	handler1 := func(w http.ResponseWriter, r *http.Request) {
+		var buf bytes.Buffer
+		enc := json.NewEncoder(&buf)
+		if err := enc.Encode(&tasks); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(buf.String())
+
+		_, err := fmt.Fprint(w, buf.String())
+		if err != nil {
+			return
+		}
+	}
+
+	// GET /tasks
+	http.HandleFunc("/tasks", handler1)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
